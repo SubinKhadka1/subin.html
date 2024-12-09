776dfcaf1fc1fd922d5b0319bc0aecf6b5b1bfe9
SubinKhadka1 <khadkasubin14@gmail.com>
2024-12-09 11:30:20 +0545

    css added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const a = 20;
-const b = 10;
-console.log(`The sum of ${a} and ${b} is ${a + b}`);
+// const a = 20;
+// const b = 10;
+// console.log(`The sum of ${a} and ${b} is ${a + b}`);
+
+array = [1, 2, 3, 4, 5, 6];
+// console.log(array[2]);
+// console.log(array[5]);
+
+const [a, b, c, d, e, f] = array;
+console.log(d);
